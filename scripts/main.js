84a094aac6f1d3d54f6a70a0d78700349dbb2414
EDIT L28 <geral@EDIT-L28.local>
2016-05-20 22:55:40 +0100

    tentando fazer slider...

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,19 @@ $(document).ready(function(){
 	});
 
 
-	$('#slider-details').slick({
+	$('.slider-details').slick({
 	  infinite: false,
 	  prevArrow: '#prev-lg',
 	  nextArrow: '#next-lg'
 	});
+    
+
+    
+    $('.details-tab nav ul li').each( function( i ) {
+
+      $('.tab-0' + (i+1) + ' a').click(function(){
+        alert('.tab-0' + (i+1) + ' a');
+        $('.slider-details').slickGoTo(3);
+      })
+    })
 });
